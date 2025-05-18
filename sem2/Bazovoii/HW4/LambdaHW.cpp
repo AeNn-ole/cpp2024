7c52236c04ede95421ab2e68544bdad2810f3d4c
Evgenii Bazovoi <bazovoyz@mail.ru>
2025-05-18 20:01:39 +0300

    Create MINIHW4
    Базовой Евгений Вадимович
    МиниДЗ 4

---
+++ sem2/Bazovoii/HW4/LambdaHW.cpp
@@ -0,0 +1,19 @@
+﻿#include <vector>
+#include <iostream>
+
+int main()
+{
+    std::vector <std::string> strs = { "kakashke", "bugagashke" }; 
+    auto count_char = [](const std::vector<std::string>& strings) { 
+        std::vector <size_t> lengts;
+        for (const auto& str : strings) {
+            lengts.push_back(str.size());
+        }
+        return lengts;
+        };
+    std::vector<size_t> res = count_char(strs);
+    for (size_t len : res) {
+        std::cout << len << " " << std::endl;
+    }
+}
+
